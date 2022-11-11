9676409d1eea306e581c79812b71e3933d45d6c6
Sizhan Xu <sizhan_x@utexas.edu>
2022-11-10 20:45:15 -0600

    running jpf-shadow from CLEVER_JAVA

--- CLEVER-JAVA/src/main/org/softwaretesting/Main.java
+++ CLEVER-JAVA/src/main/org/softwaretesting/Main.java
@@ -1,9 +1,15 @@
 package org.softwaretesting;
 
+import gov.nasa.jpf.shadow.SymExParameter;
 
 
 public class Main {
     public static void main(String[] args) {
       System.out.println("this is a test!");
+      SymExParameter Add = new SymExParameter("${jpf-shadow}/build/jpf-shadow.jar",
+            "${jpf-shadow}/src/examples", "jpf2019.add", "Add", "add", "add(sym#sym)", 1, "",
+            "evaluation-results/04_ADD_JPF/shadow-results/", null);
+
+      System.out.println(Add);
     }
 }
